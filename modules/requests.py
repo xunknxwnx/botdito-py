20268d40647465d5b80899aed034ba0f4e797a9c
unknxnwn <34998942+xunknxwnx@users.noreply.github.com>
2019-04-25 14:28:56 -0500

    Update requests.py

--- modules/requests.py
+++ modules/requests.py
@@ -2,4 +2,18 @@ import discord
 from discord.ext import commands
 
 class Requests(commands.Cog, name='Requests'):
-  
+  def __init__(self,bot):
+    self.bot=bot
+    
+  @commands.group(name="friendcode",aliases=['fc'])
+  async def fc(self,ctx):
+    if ctx.invoked_subcommand==None:
+      await ctx.send("Command is under construction")
+      
+  @fc.command()
+  async def view(self,ctx,user=None,console=None):
+    valid_consoles=['3ds','switch']
+    
+      
+def setup(bot):
+  bot.add_cog(Requests(bot))
